bf354c7b51bb0368cbd65c950b113cc36f93afcf
Mauricio Abello <edermauricioa@gmail.com>
2020-11-11 08:57:11 -0500

    Changes into the implementation

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ import picamera.array
 import cv2
 import numpy as np
 import io
+from gpiozero import LED
 from threading import Condition
+from http import server
+import socketserver
+
 
 # Global constants
 output_width = 1280
 output_height = 960
+
+IR_led = LED(20)
+np_image = None
 
 
 class StreamingOutput(object):
@@ -19,6 +26,7 @@ class StreamingOutput(object):
         self.plate_cascade = plate_cascade
 
     def write(self, buf):
+        global np_image
         # Here the frame is written in the program
         # We must perform some kind of processing
         # To check if a plate is detected
@@ -48,8 +56,43 @@ class StreamingOutput(object):
         return self.buffer.write(buf)
 
 
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/test':
+            content = bytes('Hello world', 'utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/snapshot':
+            if np_image is None:
+                print('Numpy image is none - Sending 500 error')
+                self.send_error(500)
+                self.end_headers()
+            else:
+                _, jpeg_np = cv2.imencode('.JPEG', np_image)
+                jpeg_frame = jpeg_np.tobytes()
+                self.send_response(200)
+                self.send_header('Content-Type', 'image/jpeg')
+                self.send_header('Content-Length', len(jpeg_frame))
+                self.end_headers()
+                print(len(jpeg_frame))
+                self.wfile.write(jpeg_frame)
+        else:
+            self.send_error(404)
+            self.end_headers()
+
 def main():
     print('Initializing main function')
+
+    print('Turning on the led sensor')
+    # IR_led.on()
 
     # Initialize cascade classifier
     plate_cascade = cv2.CascadeClassifier()
@@ -68,9 +111,14 @@ def main():
         camera.resolution = (output_width, output_height)
         output = StreamingOutput(plate_cascade)
         camera.start_recording(output, format='bgr')
-
-        input('Press enter to stop the program...')        
-        # Waiting for enter input
+    
+        print('Starting http server')
+        try:
+            address = ('', 8000)
+            server = StreamingServer(address, StreamingHandler)
+            server.serve_forever()
+        finally:
+            camera.stop_recording()
 
 
 if __name__ == '__main__':
